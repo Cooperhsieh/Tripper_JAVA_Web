088726ba73239c60e5e997e0268de48a9ab33354
Connor <etdid66@gmail.com>
2020-09-02 14:38:00 +0800

    資料庫連線 Utail及設定檔

---
+++ src/web/com/util/ServiceLocator.java
@@ -0,0 +1,58 @@
+package web.com.util;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+* 類別說明：建立資料庫連線
+* @author Connor Fan
+* @version 建立時間:Sep 2, 2020 2:00:52 PM
+* 
+*/
+public class ServiceLocator {
+	private static final String TAG = "TAG_ServiceLocator_";
+	private Context initContext;
+	// 實體化
+	private static ServiceLocator serviceLocator = new ServiceLocator();
+	
+	// 提供外部呼叫
+	public static ServiceLocator getInstance() {
+		return serviceLocator;
+	}
+	
+	private ServiceLocator() {
+		try {
+			this.initContext = new InitialContext();
+		} catch (NamingException e) {
+			System.out.println(TAG + "ServiceLocator fail:" + e.getMessage());
+		}
+	}
+	
+	// 預設連至Tripper DB
+	public DataSource getDataSource() {
+		DataSource dataSource = null;
+		try {
+			Context cxt = (Context) initContext.lookup("java:comp/env");
+			dataSource = (DataSource) cxt.lookup("jdbc/Tripper");
+		} catch (Exception e) {
+			System.out.println(TAG + "getDataSource fail:" + e.getMessage());
+		}
+		return dataSource;
+	}
+	
+	// 依DB Name連線
+	public DataSource getDataSource(String dataSourceName) {
+		DataSource dataSource = null;
+		try {
+			Context cxt = (Context) initContext.lookup("java:comp/env");
+			dataSource = (DataSource) cxt.lookup("jdbc/" + dataSourceName);
+		} catch (Exception e) {
+			System.out.println(TAG + "getDataSource fail:" + e.getMessage());
+		}
+		return dataSource;
+	}
+	
+
+}
