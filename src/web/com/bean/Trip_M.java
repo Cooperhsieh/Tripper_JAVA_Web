f9c3164fda793783d75b03b288e2f4d7871cd6c1
Cooper <>
2020-09-03 16:36:05 +0800

    no message

---
+++ src/web/com/bean/Trip_M.java
@@ -0,0 +1,17 @@
+package web.com.bean;
+
+public class Trip_M {
+	private int tripId;
+	private int memberId;
+	private String tripTitle;
+	private String startDate;
+	private String startTime;
+	private int dayCount;
+	private String createDateTime;
+	private int pMax;
+	private int status;
+	
+	
+	
+
+}
