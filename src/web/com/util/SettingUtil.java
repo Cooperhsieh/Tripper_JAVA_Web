a0a18a493b816c74141464974924586ae83e29dc
Connor <etdid66@gmail.com>
2020-09-03 15:12:49 +0800

    常用常數設定檔， 例如 comtex_type , image/jpeg

---
+++ src/web/com/util/SettingUtil.java
@@ -0,0 +1,11 @@
+package web.com.util;
+/**
+* 類別說明：設定常數設定檔
+* @author Connor Fan
+* @version 建立時間:Sep 3, 2020 1:32:54 PM
+* 
+*/
+public class SettingUtil {
+	public static final String CONTENT_TYPE = "text/html; charset=utf-8";
+	public static final String IMAGE_JPEG= "image/jpeg";
+}
