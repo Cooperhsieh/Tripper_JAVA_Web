84e6cc13b1ef0907e6a35e81ebbd1eb188df02a9
Cooper <>
2020-10-07 21:32:44 +0800

    no message

--- src/web/com/impl/Trip_M_Dao_Impl.java
+++ src/web/com/impl/Trip_M_Dao_Impl.java
@@ -159,16 +159,10 @@ public class Trip_M_Dao_Impl implements Trip_M_Dao {
 	public List<Trip_M> getAll() {
 		List<Trip_M> tripMs = new ArrayList<Trip_M>();
 		String sql = "SELECT * FROM Tripper.Trip_M "
-<<<<<<< HEAD
 				+ " left join Trip_Group on Trip_M.TRIP_ID = Trip_Group.TRIP_ID "
 				+ " where STATUS = 1 " +
 				" order by M_DATETIME desc" ;
-=======
-				+ "left join Trip_Group on Trip_M.TRIP_ID = Trip_Group.TRIP_ID "
-				+ "where STATUS = 1 " +
-				"order by M_DATETIME desc" ;
-				
->>>>>>> 5675a932d2115c492d2327e1342521fb6486b4ff
+
 		try (Connection connection = dataSource.getConnection();
 				PreparedStatement ps = connection.prepareStatement(sql);) {
 			
